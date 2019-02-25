eb103573a79d06351fbd1ea8ac68d8b76ed8b724
scarlett <youjin9200@naver.com>
2019-02-25 14:09:48 +0900

    Create pro_day1_E00_K번째최대값찾기.java

---
+++ pro_day1_E00_K번째최대값찾기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+/*
+pro대비 Day1
+E00 - K 번째 최대값 찾기 
+문제
+N개의 숫자가 주어질 때, 이 중 K번째 최댓값을 찾는 프로그램을 작성하여라.
+입력 
+첫 번째 줄에 N과 K가 주어진다. (1 <= N <= 1,000,000) 그 후 두 번째 줄의 N개의 양의 정수가 주어진다
+출력
+K번째 최댓값을 출력한다 
+예제 
+10 3
+1 8 4 6 8 2 3 2 5 7 
+출력
+7
+*/
+public class pro_day1_E00 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+	}
+}
